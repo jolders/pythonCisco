aeda33fba909ce935b0070e241b59cb25c374dc0
jolders <jonathan42@primescope.co.uk>
2021-12-07 21:51:35 +0000

    Telnet Loop added

---
+++ telnetExampleLoop.py
@@ -0,0 +1,32 @@
+#! /bin/bash/python3
+# Telnetexample
+import getpass
+import telnetlib
+
+for Routers in range(2):
+    HOST = input("Enter the IP Address of Device: ")
+    print("I am logging int to Router - " + (HOST))
+    tn = telnetlib.Telnet(HOST)
+    user = input("Enter your cisco username: ")
+    password = getpass.getpass()
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"terminal len 0\n")
+    tn.write(b"configure terminal\n")
+    # tn.write(b"hostname RTR1\n")
+    tn.write(b"router eigrp 100\n")
+    tn.write(b"no auto-summary\n")
+    tn.write(b"network 0.0.0.0\n")
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+    tn.write(b"show ip protocol\n")
+    tn.write(b"show version\n")
+    tn.write(b"show ip interface brief\n")
+    tn.write(b"write memory\n")
+    tn.write(b"exit\n")
+
+    print(tn.read_all().decode('ascii'))
