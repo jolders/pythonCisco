fd6d2d3e8807bc27986eb47d75858ac2c9bfa97e
jolders <jonathan42@primescope.co.uk>
2021-12-07 20:12:45 +0000

    telnet initial commit

---
+++ telnetExample.py
@@ -0,0 +1,32 @@
+#! /bin/bash/python3
+# Telnetexample
+import getpass
+import telnetlib
+
+HOST = input("Enter the IP Address of Device: ")
+# user = input("Enter your GNS3 account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+# tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"terminal len 0\n")
+tn.write(b"configure terminal\n")
+tn.write(b"hostname RTR11\n")
+# tn.write(b"router eigrp 100\n")
+# tn.write(b"no auto-summary\n")
+# tn.write(b"network 0.0.0.0\n")
+tn.write(b"exit\n")
+tn.write(b"exit\n")
+tn.write(b"show ip protocol\n")
+tn.write(b"show version\n")
+tn.write(b"write memory\n")
+tn.write(b"exit\n")
+
+
+print(tn.read_all().decode('ascii'))
