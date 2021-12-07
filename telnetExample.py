9da9865acecf88d6e2351676c0bf46298152681e
jolders <jonathan42@primescope.co.uk>
2021-12-07 20:37:11 +0000

    Telnet now working

--- telnetExample.py
+++ telnetExample.py
@@ -4,20 +4,22 @@ import getpass
 import telnetlib
 
 HOST = input("Enter the IP Address of Device: ")
-# user = input("Enter your GNS3 account: ")
+user = input("Enter your cisco username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
 tn.read_until(b"Username: ")
-# tn.write(user.encode('ascii') + b"\n")
+
+tn.write(user.encode('ascii') + b"\n")
+
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"terminal len 0\n")
 tn.write(b"configure terminal\n")
-tn.write(b"hostname RTR11\n")
+tn.write(b"hostname RTR1\n")
 # tn.write(b"router eigrp 100\n")
 # tn.write(b"no auto-summary\n")
 # tn.write(b"network 0.0.0.0\n")
@@ -27,6 +29,5 @@ tn.write(b"show ip protocol\n")
 tn.write(b"show version\n")
 tn.write(b"write memory\n")
 tn.write(b"exit\n")
-
 
 print(tn.read_all().decode('ascii'))
