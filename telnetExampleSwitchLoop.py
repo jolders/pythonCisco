ae1ca1dceb0c4321c0df1e6201ccde3917e86811
jolders <jonathan42@primescope.co.uk>
2021-12-08 10:21:40 +0000

    Switch Loop added

---
+++ telnetExampleSwitchLoop.py
@@ -0,0 +1,29 @@
+#! /bin/bash/python3
+# Telnetexample connecting to a switch and configuring vlans
+import getpass
+import telnetlib
+
+HOST = input("Enter the IP Address of Device: ")
+user = input("Enter your cisco username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"terminal len 0\n")
+tn.write(b"configure terminal\n")
+for x in range (10,51): #VLAN10 -> VLAN50
+    tn.write("vlan{}\n".format(x).encode())
+    tn.write("name VLAN{}\n".format(x).encode())
+
+
+tn.write(b"exit\n")
+tn.write(b"write memory\n")
+
+print(tn.read_all().decode('ascii'))
+print("Addition of VLANS to a switch has completed")
