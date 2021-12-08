04a2c3051a89c7a23ea47366b47e492c6ed5b0f9
jolders <jonathan42@primescope.co.uk>
2021-12-08 16:03:48 +0000

    Changed the Telnet Loop for more user interactivity

--- telnetExampleSwitchLoop.py
+++ telnetExampleSwitchLoop.py
@@ -2,34 +2,51 @@
 # Telnetexample connecting to a switch and configuring vlans
 import getpass
 import telnetlib
+import time
+import os
 
-HOST = input("Enter the IP Address of Device: ")
-user = input("Enter your cisco username: ")
-password = getpass.getpass()
+ipaddress = input("Enter the IP Address of Device: ")  # example
+print("I'll attempt to ping: " + ipaddress)
+response = os.system("ping -c 3 " + ipaddress)
 
-tn = telnetlib.Telnet(HOST)
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
+#and then check the response...
+if response == 0:
+    print(ipaddress, 'is up!')
+    print("The current time is:", time.asctime())
 
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+    user = input("Enter your cisco username: ")
+    password = getpass.getpass()
 
+    tn = telnetlib.Telnet(ipaddress)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
 
-tn.write(b"terminal len 0\n")
-tn.write(b"configure terminal\n")
-for x in range(10, 30):  # VLAN10 -> VLAN30
-    tn.write("vlan {}\n".format(x).encode())
-    tn.write("name VLAN{}\n".format(x).encode())
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
 
+    tn.write(b"terminal len 0\n")
+    tn.write(b"configure terminal\n")
+    for x in range(10, 31):  # VLAN10 -> VLAN30
+        tn.write("vlan {}\n".format(x).encode())
+        tn.write("name VLAN{}\n".format(x).encode())
 
-tn.write(b"exit\n")
-tn.write(b"exit\n")
-tn.write(b"write memory\n")
-tn.write(b"exit\n")
-tn.write(b"exit\n")
+        tn.write(b"exit\n")
+        tn.write(b"exit\n")
+        tn.write(b"write memory\n")
+        tn.write(b"exit\n")
+        tn.write(b"exit\n")
 
-print(tn.read_all().decode('ascii'))
+        str_object = ("VLAN{} was added to the switch".format(x).encode())
+        #str_object = str_object.decode()
+        print(str_object.decode())
+        print(tn.read_all().decode('ascii'))
+    print("The loop ended here")
+
+else:
+    print(ipaddress, 'is down!')
+
+print("\n")
 print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-print("Addition of VLANS to a switch has completed.")
+print("The script ended.")
 print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
